8743802de1933615164e4c9cae2ae2dcb27f1802
Victoria Wika <victoriawika@icloud.com>
2021-02-16 16:05:03 +0100

    Add Show Details Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import './App.css'
 import Card from './Card'
 import { results } from './rickandmortyapi.json'
 
 function App() {
   return (
     <div className="App">
-      {results.map(({ name, species, image, id }) => (
-        <Card key={id} name={name} species={species} image={image} />
-      ))}
+      {results.map(
+        ({ name, species, image, status, gender, origin, location, id }) => (
+          <Card
+            key={id}
+            name={name}
+            species={species}
+            image={image}
+            status={status}
+            gender={gender}
+            origin={origin.name}
+            location={location.name}
+          />
+        )
+      )}
     </div>
   )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,43 @@
+import { useState } from 'react'
+import './Button.css'
 import './Card.css'
 
-export default function Card({ name, species, image }) {
+export default function Card({
+  name,
+  species,
+  image,
+  status,
+  gender,
+  origin,
+  location,
+}) {
   const icon = species === 'Human' ? '👨🏼' : '👽'
+  const [isShowingDetails, setIsShowingDetails] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
+
   return (
     <section className="Card">
       <h2>{name}</h2>
       {icon}
       <p>{species}</p>
       <img src={image} alt="" />
+      <button
+        onClick={event => {
+          console.log('Hello')
+          event.stopPropagation()
+          setIsClicked(!isClicked)
+          setIsShowingDetails(!isShowingDetails)
+        }}
+        className="Button"
+      >
+        {isClicked ? 'Hide Details' : 'Show Details'}
+      </button>
+      <ul hidden={!isShowingDetails}>
+        <li>Status: {status} </li>
+        <li>Gender: {gender}</li>
+        <li>Origin: {origin} </li>
+        <li>Location: {location}</li>
+      </ul>
     </section>
   )
 }
